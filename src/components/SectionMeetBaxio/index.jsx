84aaa3beb0958ba4206f3b27e7a7bf7d4654333f
gneris177 <gneris177@gmail.com>
2023-09-29 01:00:24 -0300

    fix: ajustes

--- src/components/SectionMeetBaxio/index.jsx
+++ src/components/SectionMeetBaxio/index.jsx
@@ -1,8 +1,8 @@
-import Container from '../Container'
-import './style.css'
-import play from '../../assets/images/PLAY1_foto.png'
-import logoIngresse from '../../assets/images/ingresse.png'
-import imageGrafismo from '../../assets/images/GRAFISMO.png'
+import Container from '../Container';
+import './style.css';
+import play from '../../assets/images/PLAY1_foto.png';
+import logoIngresse from '../../assets/images/ingresse.png';
+import imageGrafismo from '../../assets/images/GRAFISMO.png';
 
 const SectionMeetBaxio = () => {
   return (
@@ -10,29 +10,34 @@ const SectionMeetBaxio = () => {
       <div className="meet-baxio__content">
         <Container>
           <div style={{ position: 'relative' }}>
-          <div className="wrapper-ingresse">
-            <p className="wrapper-ingresse__text">
-            Diferente de tudo que você já viveu!
-            Seja bem-vindo ao Réveillon dos Quereres 2024, que estreia em Baixio. Localizado a pouco mais de uma hora de Salvador, próximo à Praia do Forte e à Costa do Sauípe. Com suas águas cristalinas do mar, rio e lagoas, Baixio se torna um verdadeiro refúgio para aqueles que são apaixonados pela natureza.
-            O Réveillon promete muitas emoções do dia 28 a 31/12 com atrações incríveis, eventos Open Bar e uma vibe única!
-            </p>
-            <div className="wrapper-ingresse__button">
-              <button className="button">Comprar ingresso</button>
-              <img src={logoIngresse} className='logo-ingresse' alt="logo da ingresse" />
+            <div className="wrapper-ingresse">
+              <p className="wrapper-ingresse__text">
+                Diferente de tudo que você já viveu!
+                Seja bem-vindo ao Réveillon dos Quereres 2024, que estreia em Baixio. Localizado a pouco mais de uma hora de Salvador, próximo à Praia do Forte e à Costa do Sauípe. Com suas águas cristalinas do mar, rio e lagoas, Baixio se torna um verdadeiro refúgio para aqueles que são apaixonados pela natureza.
+                O Réveillon promete muitas emoções do dia 28 a 31/12 com atrações incríveis, eventos Open Bar e uma vibe única!
+              </p>
+              <div className="wrapper-ingresse__button">
+                <button className="button">Comprar ingresso</button>
+                <img src={logoIngresse} className='logo-ingresse' alt="logo da ingresse" />
+              </div>
             </div>
+
+            <div className="wrapper-video">
+              <span className="wrapper-video__title">Conheca Baixo</span> <br />
+              <div className='column'>
+                <img src={play} alt="" />
+                <button className="button">Saiba mais</button>
+              </div>
+            </div>
+
+            <div className='meet-baixio__backgroung'></div>
           </div>
-    
-          <div className="wrapper-video">
-            <span className="wrapper-video__title">Conheca Baixo</span> <br />
-            <img src={play} alt="" />
-          </div>
-          <div className='meet-baixio__backgroung'></div>
-        </div>
         </Container>
-        {/* <img className='img' src={imageGrafismo} alt="" srcset="" /> */}
+
+        <img className='img-grafismo' src={imageGrafismo} alt="imagem ilustrativa" />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SectionMeetBaxio
+export default SectionMeetBaxio;
