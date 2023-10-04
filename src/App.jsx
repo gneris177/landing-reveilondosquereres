414a3c6602aed3b29563cd1622b09a254cd73cd9
gneris177 <gneris177@gmail.com>
2023-10-04 11:41:07 -0300

    fix: ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
-import { useEffect, useState } from 'react'
-import Header from './components/Header'
-import SectionLineUp from './components/SectionLineUp'
-import SectionMeetBaxio from './components/SectionMeetBaxio'
-import SectionReserve from './components/SectionReserve'
-import SectionMaps from './components/SectionMaps'
-import SectionAttractions from './components/SectionAttractions'
-import SectionPosts from './components/SectionPosts'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import SectionLineUp from './components/SectionLineUp';
+import SectionMeetBaxio from './components/SectionMeetBaxio';
+import SectionReserve from './components/SectionReserve';
+import SectionMaps from './components/SectionMaps';
+import SectionAttractions from './components/SectionAttractions';
+import SectionPosts from './components/SectionPosts';
+import Footer from './components/Footer';
 import './App.css'
 
-
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false)
-
   return (
-    <main className="main">
-      {isLoading ? (
-        <div className="loading-container">
-          <div className="loading"></div>
-        </div>
-      ) : (
-        <>
-          <Header />
-          <SectionLineUp />
-          <SectionMeetBaxio />
-          <SectionReserve />
-          <SectionMaps />
-          <SectionAttractions />
-          <SectionPosts />
-          <Footer />
-        </>
-      )}
+    <main className='main'>
+      <Header />
+      <SectionLineUp />
+      <SectionMeetBaxio />
+      <SectionReserve />
+      <SectionMaps />
+      <SectionAttractions />
+      <SectionPosts />
+      <Footer />
     </main>
   )
 }
